4e1c2224cffb30f600325186649d4bc1362c989e
aslamhus <aslam.percy.husain@gmail.com>
2022-02-03 22:42:33 -0800

    Add note not to update pdfjs-dist package beyond ~2.6.347

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,4 @@
 rm -rf build
-echo "test"
 # FileSelect.js entry point for node package (minified, commonjs)
 npx babel src/FileSelect.js --out-file build/FileSelect.cjs --presets=@babel/preset-env --plugins=@babel/plugin-transform-modules-commonjs,@babel/plugin-syntax-dynamic-import --no-comments
 # Webpack build libraries for browser use with es6 and umd
