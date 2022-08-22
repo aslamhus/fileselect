cf3697aa2a33ffa4fc44b6169d7ed418fded5601
aslamhus <aslam.percy.husain@gmail.com>
2022-08-21 22:41:44 -0700

    Bug: fix webpack dev server infinite loop

--- src/build/webpack.dev.js
+++ src/build/webpack.dev.js
@@ -1,18 +1,35 @@
 const path = require('path');
 const common = require('./webpack.common');
 const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 process.env.NODE_ENV = 'development';
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'source-map', // source-map for no eval
+  entry: {
+    test: './test/index.js',
+  },
 
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'FileSelect test',
+      template: './test/index.html',
+      filename: './test/index.html',
+      chunks: ['test', 'FileSelect'],
+    }),
+  ],
   devServer: {
-    devMiddleware: {
-      publicPath: '/',
+    // devMiddleware: {
+    //   publicPath: '/',
+    // },
+    static: {
+      // directory: path.join(__dirname, '../../test'),
+      // publicPath: '',
     },
-    static: './',
-    open: ['./test/index.html'],
-    compress: true,
+    // open: ['./test/index.html'],
+    // compress: true,
+    // liveReload: false,
     hot: true,
     host: 'localhost',
     port: 3000,
